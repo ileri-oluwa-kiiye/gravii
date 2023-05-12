58643bf1393a9a2662b415a1b8903d9914c917a6
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2023-05-12 12:15:47 +0100

    added the hero

--- src/assets/components/hero.jsx
+++ src/assets/components/hero.jsx
@@ -1,4 +1,5 @@
 import heroimg from "../img/hero_mobile.png"
+import icon from "../img/banking.png"
 
 
 const Hero =()=>{
@@ -6,6 +7,18 @@ const Hero =()=>{
         <div className="hero">
             <div className="mobileimg">
                 <img src={heroimg} alt="" />
+            </div>
+            <div className="header_text">
+                <img src={icon} alt="" />
+                <h1>
+                    Guaranteed to give you
+                    the best experience in <span className="orange">banking</span>
+                </h1>
+                <p>
+                    There are many variations of passages of Lorem Ipsum available,
+                    but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+                </p>
+                <a href="">Get Started</a>
             </div>
         </div>
     )
